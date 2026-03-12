bb9f7c953bdf79486a210c6021007ddf33d8ecc3
Lucas Samuel Gustzaki <126416949+Lucasgustzaki@users.noreply.github.com>
2026-03-12 08:13:46 -0300

    Adicionando projeto atoresSistema

---
+++ src/main/java/com/transporte/escolar/TransporteEscolarApplication.java
@@ -0,0 +1,11 @@
+package com.transporte.escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteEscolarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransporteEscolarApplication.class, args);
+    }
+}
